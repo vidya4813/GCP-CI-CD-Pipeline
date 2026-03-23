1a89237c1f64d13b728202b092ae1b2b806cc3a6
vidya4813 <b.vidya4813@gmail.com>
2026-03-23 15:08:57 +0530

    installed docker in gcp vm

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def home(request):
         </head>
         <body>
             <div class="overlay">
-                <h1>🚀 Welcome to CI-CD Pipeline Setup using GitHub Actions</h1>
+                <h1>🚀 Welcome to CI-CD Pipeline Setup using GitHub Actions.</h1>
                 <p>This is a Django web app deployed using CI/CD pipeline.</p>
                 <p class="footer">Pipeline Ready. ✔</p>
             </div>
