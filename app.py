0c3ae0839e98fa25b9140740433733298321a52b
vidya4813 <b.vidya4813@gmail.com>
2026-03-24 16:39:12 +0530

    changed

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ urlpatterns = [
 
 
 if __name__ == '__main__':
-    execute_from_command_line([sys.argv[0], 'runserver', '0.0.0.0:8000'])
+    execute_from_command_line(sys.argv)
+    #execute_from_command_line([sys.argv[0], 'runserver', '0.0.0.0:8000'])
